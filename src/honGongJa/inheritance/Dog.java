20310cbc222b4c6c408bc812eb60d481c364296a
seryun <devseryun@gmail.com>
2021-11-26 15:53:20 +0900

    inheritance done

---
+++ src/honGongJa/inheritance/Dog.java
@@ -0,0 +1,15 @@
+package honGongJa.inheritance;
+
+public class Dog extends Animal {
+
+	public Dog() {
+		this.kind = "Æ÷À¯·ù";
+		
+	}
+	@Override
+	public void sound() {
+	System.out.println("¿Ð¿Ð");
+	}
+
+
+}
