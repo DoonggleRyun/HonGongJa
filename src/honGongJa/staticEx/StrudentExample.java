ca86d24755f830b2d076762b3896acc5438122f9
seryun <devseryun@gmail.com>
2021-12-02 22:27:44 +0900

    인프런 https://www.inflearn.com/course/%EC%9E%90%EB%B0%94-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EC%9E%85%EB%AC%B8 강의로 넘어옴. 너무 좋음.

---
+++ src/honGongJa/staticEx/StrudentExample.java
@@ -0,0 +1,12 @@
+package honGongJa.staticEx;
+
+public class StrudentExample {
+
+	public static void main(String[] args) {
+		Student james = new Student();
+		System.out.println(james.serialNum);
+		
+		Student lora = new Student();
+		System.out.println(lora.studentID);
+	}
+}
